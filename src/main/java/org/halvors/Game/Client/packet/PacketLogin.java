d3756b3c020ae88fe2fc46531dd07e70de3a85d0
Halvor Lyche Strandvoll <halvors@skymiastudios.com>
2011-10-14 23:18:55 +0200

    Added .gitignore for prevent .class files and other crap from zenoy's computer to be included in the commit D:

--- src/main/java/org/halvors/Game/Client/packet/PacketLogin.java
+++ src/main/java/org/halvors/Game/Client/packet/PacketLogin.java
@@ -5,6 +5,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 public class PacketLogin extends Packet {
+	
 	@Override
 	public void readPacketData(DataInputStream in) throws IOException {
 		
