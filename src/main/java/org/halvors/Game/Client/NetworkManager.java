c50625e65909a8bd537b606e6562bf4717925f21
markus <markus@markus-XFX-nForce-790i-Ultra-3-Way-SLI.(none)>
2011-10-14 19:26:25 +0200

    socket add

--- src/main/java/org/halvors/Game/Client/NetworkManager.java
+++ src/main/java/org/halvors/Game/Client/NetworkManager.java
@@ -1,5 +1,7 @@
 package main.java.org.halvors.Game.Client;
 
+
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,6 +10,8 @@ import main.java.org.halvors.Game.Client.packet.Packet;
 
 public class NetworkManager {
 	private final List<Packet>packets = Collections.synchronizedList(new ArrayList<Packet>());
+	Socket socket = new Socket();
+	
 	
 	public NetworkManager() {
 		
@@ -16,6 +20,7 @@ public class NetworkManager {
 	public void sendPacket(Packet packet) {
 		if (packet != null) {
 			packets.add(packet);
+			
 		}
 	}
 }
