6aed8ae016c8bc0d6fcd32036d7b18e6144d4a88
sosser <sosser@skymiastudios.com>
2011-10-14 23:23:49 +0200

    - GUI stuff

--- src/main/java/org/halvors/Game/Client/gui/Menu.java
+++ src/main/java/org/halvors/Game/Client/gui/Menu.java
@@ -1,14 +1,53 @@
 package main.java.org.halvors.Game.Client.gui;
 
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/*
 import java.awt.Button;
 import java.awt.TextField;
+*/
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class Menu extends JFrame {
 	private static final long serialVersionUID = 1822019861952002123L;
 
+	private JLabel lblTitle;
+	private JButton btnConnect;
+	private JTextField txtConnectTo;
+	
+	ActionListener actionListener = new ActionListener() {
+	      public void actionPerformed(ActionEvent actionEvent) {
+	        System.out.println("ACTION!");
+	        lblTitle.setText("NO NO NO, YOU DIRTY PIG!");
+	      }
+	    };
+	
 	public Menu() {
+		
+		// Intializing stiff kinda. Tittel og flowlayout
+		super("Hey!");
+		setLayout(new FlowLayout());
+		
+		lblTitle = new JLabel("This is a test label");
+		txtConnectTo = new JTextField(20);
+		
+		btnConnect = new JButton("Join");
+		btnConnect.addActionListener(actionListener);
+		
+		add(lblTitle);
+		add(txtConnectTo);
+		add(btnConnect);
+		
+		
+		/*
 		setTitle("Game");
 		setSize(800, 600);
 		setVisible(true);
@@ -22,5 +61,7 @@ public class Menu extends JFrame {
 		add(hostField);
 		add(portField);
 		add(button);
+		*/
 	}
+
 }
