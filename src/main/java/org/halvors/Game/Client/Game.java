babe2505d68731d9c89373989adb8dc6e4bf115d
halvors <halvors@skymiastudios.com>
2011-11-03 00:00:57 +0100

    Runnable :D

--- src/main/java/org/halvors/Game/Client/Game.java
+++ src/main/java/org/halvors/Game/Client/Game.java
@@ -1,13 +1,19 @@
 package org.halvors.Game.Client;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.halvors.Game.Client.gui.MainWindow;
+import javax.imageio.ImageIO;
+
 import org.halvors.Game.Client.network.NetworkManager;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
@@ -15,7 +21,7 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 
-public class Game {
+public class Game implements Runnable {
 	private static Game instance;
 	
 	private final String name = "Game";
@@ -40,18 +46,18 @@ public class Game {
 	}
 	
 	public void main(String[] args) {
-		MainWindow mainWindow = new MainWindow(this);
+//		MainWindow mainWindow = new MainWindow(this);
 		
-//		initialize();
+		initialize();
 	}
 	
 	private void initialize() {
-		Display.setTitle(name + " " + version);
-		
 		try {
 			// Initialize Display.
-			Display.setDisplayMode(new DisplayMode(width, height));
+			
 			Display.create();
+			Display.setDisplayMode(new DisplayMode(width, height));
+			Display.setTitle(name + " " + version);
 			
 			// Initialize Keyboard and Mouse.
 			Keyboard.create();
@@ -70,6 +76,10 @@ public class Game {
 			// TODO: Render OpenGL here
 			loadScreen();
 			
+			// Update keys.
+			keyManager.onUpdate();
+			
+			// Update display.
 			Display.update();
 			
 			// FPS limit to 60.
@@ -77,6 +87,11 @@ public class Game {
 		}
 		
 		Display.destroy();
+	}
+	
+	@Override
+	public void run() {
+		
 	}
 	
 	private void loadScreen() {
@@ -162,6 +177,35 @@ public class Game {
 		
 		if (world != null) {
 			worlds.remove(world);
+		}
+	}
+	
+	public void takeScreenshot(File file) {
+		GL11.glReadBuffer(GL11.GL_FRONT);
+		int width = Display.getDisplayMode().getWidth();
+		int height = Display.getDisplayMode().getHeight();
+		int bpp = 4; // Assuming a 32-bit display with a byte each for red, green, blue, and alpha.
+		ByteBuffer buffer = BufferUtils.createByteBuffer(width * height * bpp);
+		GL11.glReadPixels(0, 0, width, height, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer);
+		
+//		File file = new File("screenshot.png"); // The file to save to.
+		String format = "png"; // Example: "PNG" or "JPG"
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		 
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				int i = (x + (width * y)) * bpp;
+		 		int r = buffer.get(i) & 0xFF;
+		 		int g = buffer.get(i + 1) & 0xFF;
+		 		int b = buffer.get(i + 2) & 0xFF;
+		 		image.setRGB(x, height - (y + 1), (0xFF << 24) | (r << 16) | (g << 8) | b);
+		 	}
+		}
+		
+		try {
+			ImageIO.write(image, format, file);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 	
--- src/main/java/org/halvors/Game/Client/KeyManager.java
+++ src/main/java/org/halvors/Game/Client/KeyManager.java
@@ -1,10 +1,48 @@
 package org.halvors.Game.Client;
 
+import java.io.File;
+import java.util.logging.Level;
+
+import org.lwjgl.input.Keyboard;
+
 public class KeyManager {
 	private final Game client;
 	
 	public KeyManager(Game client) {
 		this.client = client;
+	}
+	
+	public void onUpdate() {
+		while (Keyboard.next()) {
+			if (Keyboard.getEventKeyState()) {
+				// Key was pressed.
+				
+				if (Keyboard.getEventKey() == Keyboard.KEY_W) {
+					
+				}
+				
+				if (Keyboard.getEventKey() == Keyboard.KEY_A) {
+					
+				}
+				
+				if (Keyboard.getEventKey() == Keyboard.KEY_S) {
+					
+				}
+				
+				if (Keyboard.getEventKey() == Keyboard.KEY_D) {
+					
+				}
+				
+				if (Keyboard.getEventKey() == Keyboard.KEY_F1) {
+					File file = new File("Screenshot.png");
+					
+					client.log(Level.INFO, "Saving screenshot to: " + file.getAbsolutePath());
+					client.takeScreenshot(file);
+				}
+			} else  {
+				// Key was released.
+			}
+		}
 	}
 
 	public Game getClient() {
